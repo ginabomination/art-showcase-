349a32bdf25b1a675c13a5178df35b76aeeeac3c
ginabomination <grugerstorz@gmail.com>
2022-04-04 10:27:21 +1200

    flexxing

--- m.js
+++ m.js
@@ -13,3 +13,30 @@ function toggleSidebar() {
     this.mini = true;
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+var mini = true;
+      
+        function toggleSidebar() {
+          if (mini) {
+            document.getElementById("mySidebar").style.width = "250px";
+            document.getElementById("main").style.marginLeft = "250px";
+            this.mini = false;
+          } 
+      
+          else {
+            document.getElementById("mySidebar").style.width = "85px";
+            document.getElementById("main").style.marginLeft = "85px";
+            this.mini = true;
+          }
+        }
